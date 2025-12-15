8c8dedfdd942dfa4bb4088cdbc1dfc9763b793c7
Corey Tarpley <developertarpley@gmail.com>
2025-12-15 02:00:58 -0700

    hotel feedback form:rwd/html and js variable:js/vars and stringss

---
+++ JavaScript/variablesAndstrings/sentenceMaker.js
@@ -0,0 +1,35 @@
+// User Stories:
+
+// You should declare the following variables using let:
+
+const adjective = "";
+const noun = "";
+const verb = "";
+const place = "";
+const adjective2 = "";
+const noun2 = "";
+// const You should assign the above variables some string values of your choice.
+
+// You should declare a firstStory variable.
+
+// You should use the following story template to create the first story and assign it to the firstStory variable: "Once upon a time, there was a(n) [adjective] [noun] who loved to eat [noun2]. The [noun] lived in a [place] and had [adjective2] nostrils that blew fire when it was [verb].";
+const firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+// You should output your first story to the console using the message "First story: [firstStory]".
+console.log(firstStory);
+// You should assign new values to your adjective, noun, verb, place, adjective2, and noun2 variables.
+
+adjective = "";
+noun = "";
+verb = "";
+place = "";
+adjective2 = "";
+noun2 = "";
+
+// You should declare a secondStory variable.
+
+// Create another story using the same template and assign it to the secondStory variable.
+const secondStory =`Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+// You should output your second story to the console using the message "Second story: [secondStory]".
+
+console.log(secondStory);
---
+++ JavaScript/variablesAndstrings/triviaBot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "The One Who Could Help";
+const botLocation = "Dev Operating System";
+const favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `- Did you know that ${favoriteLanguage} is a must know cornerstone for modern web developement today? It is used to bring functionality to the website we have fun interacting with! -`;
+
+console.log(codingFact);
+
+codingFact = `
+- Many large tech companies run on JS: Sites like Google, YouTube, Facebook, Amazon, and Twitter all rely heavily on ${favoriteLanguage}. - 
+`
+console.log(codingFact);
+
+codingFact = `- There are ${favoriteLanguage} frameworks like React for Meta's Facebook, Angular for Google, or even the unseen(backend developed Node and Express) code used by LinkedIn and PayPal. -`
+
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
